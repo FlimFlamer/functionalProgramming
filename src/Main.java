56ff02df419033a809a5ce785648884215f3290c
FlimFlamer <dronik61@gmail.com>
2023-03-10 22:40:51 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        WordCount count = new WordCount(scan());
+        count.printResult();
+    }
+
+    public static String scan() {
+        System.out.println("Введите строку: ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 }
